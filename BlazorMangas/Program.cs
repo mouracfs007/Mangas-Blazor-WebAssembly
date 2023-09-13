3af26e5144cc81fd08a1c18bc42256d49c2fffc2
Mouracfs <danielmourajuve10@gmail.com>
2023-09-13 17:31:10 -0300

    Adding class: Mangas, MangaService

--- BlazorMangas/Program.cs
+++ BlazorMangas/Program.cs
@@ -22,6 +22,7 @@ builder.Services.AddAuthorizationCore();
 builder.Services.AddScoped<AuthenticationStateProvider, ApiAuthenticationStateProvider>();
 builder.Services.AddScoped<IAuthService, AuthService>();
 builder.Services.AddScoped<ICategoriaService, CategoriaService>();
+builder.Services.AddScoped<IMangaService, MangaService>();
 
 
 await builder.Build().RunAsync();
