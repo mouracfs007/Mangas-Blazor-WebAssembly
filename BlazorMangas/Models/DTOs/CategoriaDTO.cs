a47a919438b8d40b5acfb36acbd345876bece77d
Mouracfs <danielmourajuve10@gmail.com>
2023-09-15 13:36:30 -0300

    Catalog Mangás, modifications in entity class, services

--- BlazorMangas/Models/DTOs/CategoriaDTO.cs
+++ BlazorMangas/Models/DTOs/CategoriaDTO.cs
@@ -7,4 +7,6 @@ public class CategoriaDTO
     public int Id { get; set; }
     [Required(ErrorMessage = "O nome é requerido")]
     public string? Nome { get; set; }
+    [Required(ErrorMessage = "O nome é requerido")]
+    public string? IconCSS { get; set; }
 }
